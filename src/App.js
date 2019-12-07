56ffd424800925588f0e9471185840ee427a84de
Robert Jenkins <bobert@gmail.com>
2019-12-07 15:04:00 -0500

    refactored code to use functional components and hooks

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import CloudFile from './CloudFile'
-import TreeNode from './TreeNode'
+import CloudFile from './CloudFile';
+import TreeNode from './TreeNode';
+import TreeRoot from './TreeRoot';
 import './App.css';
-import sampleData from './SampleData';
-// import axios from 'axios';
-import API from './API'
+import API from './API';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      treeNodeComponents: null
-    }
-
-  }
-
-  async componentDidMount() {
-    const treeRoot = await API.get('folders'); //https://designrevision.com/react-axios/
-    const treeNodeComponents = treeRoot.data.data.map( node => <TreeNode key={node.vue_id} node={node} />)
-    this.setState({
-      isLoading: false,
-      treeNodeComponents: treeNodeComponents
-    })
-  }
-
-  render() {
-    return (
-      <ul className="App">
-        { this.state.isLoading ? <li>Loading....</li> : this.state.treeNodeComponents }
-      </ul>
-    );
-  }
+function App() {
+  return (
+    <TreeRoot />
+  );
 }
 
 export default App;
---
+++ src/TreeRoot.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import TreeNode from './TreeNode';
+import API from './API'
+
+function TreeRoot() {
+  const [ isLoading, setIsLoading ] = useState(true);
+  const [ treeNodeComponents, setTreeNodeComponents] = useState([]);
+
+  useEffect( async () => {
+    const treeRoot = await API.get('folders'); //https://designrevision.com/react-axios/
+    const treeNodeComponents = treeRoot.data.data.map( node => <TreeNode key={node.vue_id} node={node} />)
+    setTreeNodeComponents(treeNodeComponents);
+    setIsLoading(false)
+  }, [])
+
+  return (
+    <ul className="TreeRoot">
+      { isLoading ? <li>Loading....</li> : treeNodeComponents }
+    </ul>
+  );
+}
+
+export default TreeRoot;
